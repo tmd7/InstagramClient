6413669db77eb92c776523b2993ecb1f2f0735e4
Marat Tlepberghenov <mur.marat@gmail.com>
2018-08-22 13:24:52 +0300

    Has deleted Base Activity

--- app/src/main/java/com/tmarat/instagramclient/main/MainActivity.java
+++ app/src/main/java/com/tmarat/instagramclient/main/MainActivity.java
@@ -2,39 +2,32 @@ package com.tmarat.instagramclient.main;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.tmarat.instagramclient.R;
-import com.tmarat.instagramclient.base.BaseActivity;
 import com.tmarat.instagramclient.ui.MainFragment;
 import com.tmarat.instagramclient.ui.SettingsFragment;
 import com.tmarat.instagramclient.util.ActivityUtils;
 
-public class MainActivity extends BaseActivity implements MainContract.View {
+public class MainActivity extends AppCompatActivity implements MainContract.View {
 
-  @Override protected void init(@Nullable Bundle state) {
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
     setUpToolbar();
 
-    if (state == null) {
+    if (savedInstanceState == null) {
       ActivityUtils.replaceFragmentToActivity(getSupportFragmentManager(),
           MainFragment.newInstance(), R.id.main_container);
     }
-  }
-
-  @Override protected int getContentResource() {
-    return R.layout.activity_main;
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.menu_main, menu);
     return true;
-  }
-
-  @Override protected void injectDependencies() {
-
   }
 
   @Override protected void onDestroy() {
