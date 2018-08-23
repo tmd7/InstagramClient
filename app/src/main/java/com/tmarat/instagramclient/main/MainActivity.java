d1312c684aa91f9f7f140eef718613ae67e82dfe
Marat Tlepberghenov <mur.marat@gmail.com>
2018-08-23 11:18:30 +0300

    Added navigation drawer into main activity

--- app/src/main/java/com/tmarat/instagramclient/main/MainActivity.java
+++ app/src/main/java/com/tmarat/instagramclient/main/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tmarat.instagramclient.main;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,12 +13,15 @@ import com.tmarat.instagramclient.R;
 import com.tmarat.instagramclient.settings.SettingsFragment;
 import com.tmarat.instagramclient.util.ActivityUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
-  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    setupToolbar();
+
+    setNavigationDrawer();
 
     if (savedInstanceState == null) {
       ActivityUtils.replaceFragmentToActivity(getSupportFragmentManager(),
@@ -23,19 +29,39 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
+  private void setNavigationDrawer() {
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    drawer.addDrawerListener(toggle);
+    toggle.syncState();
+
+    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
   }
 
-  @Override protected void onDestroy() {
-    super.onDestroy();
+  @Override
+  public void onBackPressed() {
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    if (drawer.isDrawerOpen(GravityCompat.START)) {
+      drawer.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
   }
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-
     switch (item.getItemId()) {
       case R.id.action_settings:
         ActivityUtils.replaceFragmentToActivity(getSupportFragmentManager(),
@@ -49,8 +75,28 @@ public class MainActivity extends AppCompatActivity {
     return super.onOptionsItemSelected(item);
   }
 
-  private void setupToolbar() {
-    Toolbar toolbar = findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+
+      case R.id.nav_home:
+        ActivityUtils.replaceFragmentToActivity(getSupportFragmentManager(),
+            MainFragment.newInstance(), R.id.main_container);
+        break;
+
+      case R.id.nav_settings:
+        ActivityUtils.replaceFragmentToActivity(getSupportFragmentManager(),
+            SettingsFragment.newInstance(), R.id.main_container, null);
+        break;
+
+      default:
+        break;
+    }
+
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    drawer.closeDrawer(GravityCompat.START);
+    return true;
   }
 }
