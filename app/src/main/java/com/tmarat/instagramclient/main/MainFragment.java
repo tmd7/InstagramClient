39d27250f8f190a002a3acbcbb83a3aac6351c86
Marat Tlepberghenov <mur.marat@gmail.com>
2018-08-24 14:34:20 +0300

    dispatched all the methods which take pictures to View for fixed bugs. This is a temporary solution.

--- app/src/main/java/com/tmarat/instagramclient/main/MainFragment.java
+++ app/src/main/java/com/tmarat/instagramclient/main/MainFragment.java
@@ -1,21 +1,32 @@
 package com.tmarat.instagramclient.main;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import com.tmarat.instagramclient.R;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
+import static com.tmarat.instagramclient.util.ConstantsUtil.REQUEST_TAKE_PHOTO;
 
 public final class MainFragment extends Fragment implements MainContract.View {
 
   private MainContract.Presenter presenter;
-
+  private Uri photoURI;
   private ImageView imageView;
 
   public static MainFragment newInstance() {
@@ -47,11 +58,59 @@ public final class MainFragment extends Fragment implements MainContract.View {
     view.findViewById(R.id.float_bt_main_fragment)
         .setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
-        presenter.addPhoto(getContext(), getActivity());
+        dispatchTakePictureIntent();
       }
     });
 
     imageView = view.findViewById(R.id.image_view_test);
+  }
+
+  @Override public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+      imageView.setImageURI(photoURI);
+    }
+  }
+
+  private void dispatchTakePictureIntent() {
+
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+    // Ensure that there's a camera activity to handle the intent
+    if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+
+      // Create the File where the photo should go
+      File photoFile = null;
+      try {
+        photoFile = createImageFile();
+      } catch (IOException ex) {
+        showSnackbar(R.string.error);
+      }
+
+      // Continue only if the File was successfully created
+      if (photoFile != null) {
+        photoURI = FileProvider.getUriForFile(getActivity(),
+            getActivity().getPackageName() + ".fileprovider",
+            photoFile);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+      }
+    }
+  }
+
+  private File createImageFile() throws IOException {
+    // Create an image file name
+    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    String imageFileName = "JPEG_" + timeStamp + "_";
+    File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+    File image = File.createTempFile(
+        imageFileName,
+        ".jpg",
+        storageDir
+    );
+
+    // Save a file: path for use with ACTION_VIEW intents
+    //mCurrentPhotoPath = image.getAbsolutePath();
+    return image;
   }
 
   @Override public void showSnackbar(int resId) {
@@ -59,14 +118,6 @@ public final class MainFragment extends Fragment implements MainContract.View {
       Snackbar snackbar = Snackbar.make(getView(), resId, Snackbar.LENGTH_SHORT);
       snackbar.dismiss();
       snackbar.show();
-    }
-  }
-
-  @Override public void showImage(String currentPhotoPath) {
-    if (currentPhotoPath != null) {
-      imageView.setImageURI(Uri.parse(currentPhotoPath));
-    } else {
-      showSnackbar(R.string.error);
     }
   }
 }
--- app/src/main/java/com/tmarat/instagramclient/main/MainPresenter.java
+++ app/src/main/java/com/tmarat/instagramclient/main/MainPresenter.java
@@ -1,74 +1,9 @@
 package com.tmarat.instagramclient.main;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.FileProvider;
-import android.util.Log;
-import com.tmarat.instagramclient.R;
 import com.tmarat.instagramclient.base.BasePresenter;
-import com.tmarat.instagramclient.util.ConstantsUtil;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainPresenter extends BasePresenter<MainContract.View>
     implements MainContract.Presenter {
 
   private static final String TAG = MainPresenter.class.getSimpleName();
-
-  private String currentPhotoPath;
-
-  @Override
-  public String getCurrentPhotoPath() {
-    return currentPhotoPath;
-  }
-
-  @Override public void addPhoto(Context context,  FragmentActivity fragmentActivity) {
-    // TODO: 23.08.2018 Take a photography
-
-    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    if (takePictureIntent.resolveActivity(context.getPackageManager()) != null) {
-      File photoFile = null;
-      try {
-        photoFile = createImageFile(context);
-      } catch (IOException e) {
-        e.printStackTrace();
-        getView().showSnackbar(R.string.error);
-      }
-
-      if (photoFile !=null) {
-        Uri photoUri = FileProvider.getUriForFile(
-            context,
-            context.getPackageName()+".fileprovider",
-            photoFile);
-        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-        fragmentActivity.startActivityForResult(takePictureIntent, ConstantsUtil.REQUEST_TAKE_PHOTO);
-      }
-    }
-  }
-
-  private File createImageFile(Context context) throws IOException {
-
-    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-    String imageFileName = "JPEG_" + timeStamp + "_";
-    File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-    File image = File.createTempFile(
-        imageFileName,
-        ".jpg",
-        storageDir
-    );
-
-    currentPhotoPath = image.getAbsolutePath();
-
-    getView().showImage(currentPhotoPath);
-
-    Log.d(TAG, "createImageFile: photo path: " + currentPhotoPath);
-    return image;
-
-  }
 }
