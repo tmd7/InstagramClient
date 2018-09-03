3b9166def93b316c177f36b58168519c460dc9eb
Marat Tlepberghenov <mur.marat@gmail.com>
2018-09-03 12:37:53 +0300

    Added insert method

--- app/src/main/java/com/tmarat/instagramclient/data/PhotoDao.java
+++ app/src/main/java/com/tmarat/instagramclient/data/PhotoDao.java
@@ -1,6 +1,7 @@
 package com.tmarat.instagramclient.data;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import java.util.List;
 
@@ -8,4 +9,6 @@ import java.util.List;
 public interface PhotoDao {
 
   @Query("SELECT * FROM photoentity") List<PhotoEntity> getAll();
+
+  @Insert void insert(PhotoEntity photoEntity);
 }
